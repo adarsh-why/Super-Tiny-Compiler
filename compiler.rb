fa05a7f756ead50fd4760becafe91dfd5a55834a
Adarsh Chandran <adarsh.chndrn@gmail.com>
2016-04-04 11:17:46 +0530

    tokenized

---
+++ compiler.rb
@@ -0,0 +1,22 @@
+def tokenize input
+	tokens = input.gsub!('(', '( ').gsub!(')', ' )').split
+	(0...tokens.length).each do |indx|
+		case tokens[indx]
+			when '('
+			tokens[indx] = {type: 'parens', value: '('}
+
+			when ')'
+			tokens[indx] = {type: 'parens', value: ')'}
+
+			when /^([0-9])/
+			tokens[indx] = {type: 'number', value: tokens[indx]}
+
+			when /[a-z]/
+			tokens[indx] = {type: 'name', value: tokens[indx]}
+		end
+	end
+	puts tokens
+end
+
+###### Test Case for Functions ######
+tokenize '(add 2 (subtract 4 2))'
