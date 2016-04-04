3c611ba7a88ee0330c2ce97fa3248013cac553ef
Adarsh <adarsh.chndrn@gmail.com>
2016-04-04 11:21:38 +0530

    intendation fix

--- compiler.rb
+++ compiler.rb
@@ -1,21 +1,21 @@
 def tokenize input
-	tokens = input.gsub!('(', '( ').gsub!(')', ' )').split
-	(0...tokens.length).each do |indx|
-		case tokens[indx]
-			when '('
-			tokens[indx] = {type: 'parens', value: '('}
+    tokens = input.gsub!('(', '( ').gsub!(')', ' )').split
+    (0...tokens.length).each do |indx|
+        case tokens[indx]
+            when '('
+            tokens[indx] = {type: 'parens', value: '('}
 
-			when ')'
-			tokens[indx] = {type: 'parens', value: ')'}
+            when ')'
+            tokens[indx] = {type: 'parens', value: ')'}
 
-			when /^([0-9])/
-			tokens[indx] = {type: 'number', value: tokens[indx]}
+            when /^([0-9])/
+            tokens[indx] = {type: 'number', value: tokens[indx]}
 
-			when /[a-z]/
-			tokens[indx] = {type: 'name', value: tokens[indx]}
-		end
-	end
-	puts tokens
+            when /[a-z]/
+            tokens[indx] = {type: 'name', value: tokens[indx]}
+        end
+    end
+    puts tokens
 end
 
 ###### Test Case for Functions ######
