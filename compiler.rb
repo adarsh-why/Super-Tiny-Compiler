a268b647778746086579a2b6a206ae93e46b3956
Adarsh <adarsh.chndrn@gmail.com>
2016-04-04 16:39:06 +0530

    working on parsing

--- compiler.rb
+++ compiler.rb
@@ -3,20 +3,59 @@ def tokenize input
     (0...tokens.length).each do |indx|
         case tokens[indx]
             when '('
-            tokens[indx] = {type: 'parens', value: '('}
+            tokens[indx] = {type: 'paren', value: '('}
 
             when ')'
-            tokens[indx] = {type: 'parens', value: ')'}
+            tokens[indx] = {type: 'paren', value: ')'}
 
             when /^([0-9])/
             tokens[indx] = {type: 'number', value: tokens[indx]}
-
+    
             when /[a-z]/
             tokens[indx] = {type: 'name', value: tokens[indx]}
         end
     end
-    puts tokens
+    return tokens
+end
+
+def abs_tree tokens, current
+	token = tokens[current]
+
+	if token[:type] == 'number'
+		current += 1
+		return {type: 'NumberLiteral', value: token[:value]}, current
+	end
+
+	if (token[:type] == 'paren' and token[:value] == '(')
+		current += 1
+		token = tokens[current]
+		node = {type: 'CallExpression', name: token[:value], params: []}
+		token = tokens[current]
+
+		while ((token[:type] != 'paren') || (token[:type] == 'paren' && token[:value] != ')'))
+			node[:params] << abs_tree(tokens, current)
+			token = tokens[current += 1]
+		end
+
+		current += 1
+
+		return node, current
+	end
+end
+
+def parser tokens
+	
+	current = 0
+	ast = {type: 'Program', body: []}
+
+	while (current < tokens.length)
+		node, current = abs_tree(tokens, current)
+		ast[:body] << node
+	end
+
+	return ast
 end
 
 ###### Test Case for Functions ######
-tokenize '(add 2 (subtract 4 2))'
+tok = tokenize '(add 2 (subtract 4 2))'
+p parser tok
