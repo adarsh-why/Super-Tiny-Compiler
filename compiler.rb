db8b323ff1591cb06ab3bdc71f9f63b0e79502c6
Adarsh Chandran <adarsh.chndrn@gmail.com>
2016-04-07 16:11:30 +0530

    partial output

--- compiler.rb
+++ compiler.rb
@@ -4,14 +4,11 @@ def tokenize input
         case tokens[indx]
             when '('
             tokens[indx] = {type: 'paren', value: '('}
-
             when ')'
             tokens[indx] = {type: 'paren', value: ')'}
-
             when /^([0-9])/
             tokens[indx] = {type: 'number', value: tokens[indx]}
-    
-            when /[a-z]/
+            when /[a-z]/i
             tokens[indx] = {type: 'name', value: tokens[indx]}
         end
     end
@@ -23,22 +20,22 @@ def make_tree
     if token[:type] == 'number'
         @current += 1
         return {type: 'NumberLiteral', value: token[:value]}
-    end        
-    if (token[:value] == '(')
+    end
+    if token[:value] == '('
         token = @tokens[@current += 1]
-        node = {type: 'CallExpression', name: token[:value], params: []}
+        node = {type: 'CallExpression', callee: {type: 'Identifier', name: token[:value]}, arguments: []}
         token = @tokens[@current += 1]
         while token[:value] != ')'
-            node[:params] << make_tree
+            node[:arguments] << make_tree
             token = @tokens[@current]
         end
         @current += 1
         return node
-    end
+    end        
 end
 
 def parser    
-    @current = 0
+    @current = 0 
     ast = {type: 'Program', body: []}
     while (@current < @tokens.length)
         ast[:body] << make_tree
@@ -46,5 +43,23 @@ def parser
     return ast
 end
 
-@tokens = tokenize '(add 2 (subtract 4 2))'
-puts parser
+def code_generate node
+    case node[:type]
+    when 'Program'
+        return node[:body].map{|node| code_generate node}.join("\n")
+    when 'CallExpression'
+        return (code_generate(node[:callee]) + '(' + 
+            node[:arguments].map{|node| code_generate node}.join(', ') + ')')
+    when 'Identifier'
+        return node[:name]
+    when 'NumberLiteral'
+        return node[:value]
+    end
+end
+
+input = '(add 3 (subtract 4 2))'
+puts "\n\n#{input}\n\n"
+@tokens = tokenize input
+puts "#{@tokens}\n\n"
+puts "#{parser}\n\n"
+puts "#{code_generate parser}\n\n"
